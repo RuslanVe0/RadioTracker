50ee14d6e36ecfdb06f04a645775f205c480fa7a
RuslanVe0 <129605480+RuslanVe0@users.noreply.github.com>
2026-02-05 10:24:58 +0200

    Add files via upload

--- httplib/httplib.py
+++ httplib/httplib.py
@@ -3,7 +3,7 @@ import copy
 from httplib._socket import socket
 import utils.utils
 
-
+http_header_: str = "GET /%s HTTP/1.1\r\x0AHost: %s\r\x0AUser-Agent: Python/RadioTracker\r\x0AConnection: close\r\x0AAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\x0A\r\x0A"
 
 class httplib(object):
     
@@ -17,7 +17,7 @@ class httplib(object):
         return f"<httplib host={self.host} path={self.path}>"
     
     def create_request_get(self):
-        http_header: str = f"GET /{self.path} HTTP/1.1\r\x0AHost: {self.host}\r\x0AUser-Agent: Python/RadioTracker\r\x0AConnection: close\r\x0AAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\x0A\r\x0A"
+        http_header: str = http_header_ % (self.path, self.host)
         self.socket.send(http_header.encode("utf-8", errors = "ignore"))
         for _ in range(0, 2, 1):
             self.payload += self.socket.recv(8192).decode("utf-8", errors = "ignore")
@@ -29,7 +29,7 @@ class httplib(object):
 
     
     def create_request_file(self, constructor):
-        http_header: str = f"GET /{self.path} HTTP/1.1\r\x0AHost: {self.host}\r\x0AUser-Agent: Python/RadioTracker\r\x0AConnection: close\r\x0AAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\x0A\r\x0A"
+        http_header: str = http_header_ % (self.path, self.host)
         self.socket.send(http_header.encode("utf-8", errors = "ignore"))
         data: bytes = self.socket.recv(4096)
         received: bytes = data.split(b"\r\x0A\r\x0A")[1]
@@ -43,7 +43,7 @@ class httplib(object):
         return received
     
     def create_request_stream(self, constructor, writing_pipe, path: str):
-        http_header: str = f"GET /{self.path} HTTP/1.1\r\x0AHost: {self.host}\r\x0AUser-Agent: Python/RadioTracker\r\x0AConnection: close\r\x0AAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\x0A\r\x0A"
+        http_header: str = http_header_ % (self.path, self.host)
         self.socket.send(http_header.encode("utf-8", errors = "ignore"))
         data: bytes = self.socket.recv(4096).split(b"\r\x0A\r\x0A")[1]
         old_constructor = copy.deepcopy(constructor)
